818120cf3ef0330065d398dd8eebfe6467a3095e
fuckyou1907023 <ha9a1ltt@gmail.com>
2020-01-22 14:52:36 +0700

    Bai tap tet

--- Baitap/src/ex2/Fraction.java
+++ Baitap/src/ex2/Fraction.java
@@ -1,4 +1,57 @@
 package ex2;
 
+import java.util.Scanner;
+
 public class Fraction {
+    private long a;
+    private long b;
+
+    public Fraction() {
+    }
+    public Fraction(long a, long b) {
+        this.a = a;
+        this.b = b;
+    }
+    public long getA() {
+        return a;
+    }
+    public void setA(long a) {
+        this.a = a;
+    }
+
+    public long getB() {
+        return b;
+    }
+    public void setB(long b) {
+        this.b = b;
+    }
+
+    public void Nhaptuma() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Nhap canh a =");
+        a = s.nextInt();
+        System.out.println("Nhap canh b =");
+        b = s.nextInt();
+    }
+    //dieu kien
+    public boolean check(){
+        if( b != 0){
+            return true;
+        }
+        return false;
+
+    }
+
+    //so rut gon
+    public double rutGon(){
+        System.out.println(b);
+        double c =(double)(a) / (double)(b);
+        return c;
+    }
+    public void showInfor(){
+        System.out.println("phan so da nhap la:"+getA()+"/"+getB());
+        System.out.println("So nghich dao"+getB()+"/"+getA());
+        System.out.println("So rut gon la:"+rutGon());
+
+    }
 }
--- Baitap/src/ex2/Main.java
+++ Baitap/src/ex2/Main.java
@@ -1,4 +1,26 @@
 package ex2;
 
 public class Main {
+    public static void main(String[] args) {
+        Fraction p = new Fraction();
+        p.Nhaptuma();
+        p.check();
+        p.showInfor();
+        System.out.println("Nhap phan so tiep theo");
+
+
+        Fraction l = new Fraction();
+        l.Nhaptuma();
+        l.check();
+//        long tu1 =(p.getA()*l.getB());
+//        long mau1 = (p.getB()*l.getA());
+//        long tu2 =(p.getB()*l.getA());
+//        long mau2 = (p.getA()*l.getB());
+//        long tongtu = tu1 + tu2;
+//        long tongmau = mau1+mau2;
+        System.out.println("tong 2 phan so:"+((p.getA()*l.getB()) + (l.getA()*p.getB())+"/"+((l.getB()*p.getB())+(p.getB()*l.getB()))));
+        System.out.println("Tich 2 phan so:"+(p.getA()*l.getA())+"/"+(p.getB()*l.getB()));
+        System.out.println("Thuong 2 phan so"+(p.getA()*l.getB())+"/"+(p.getB()*l.getA()));
+    }
+
 }
