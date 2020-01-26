aa966a8f14d0ecbd8a97d1a63458a310b928ce7c
fuckyou1907023 <ha9a1ltt@gmail.com>
2020-01-26 08:44:54 +0700

    Nam moi binh an

---
+++ Baitap/src/Khaixuan/Khaixxuan.java
@@ -0,0 +1,7 @@
+package Khaixuan;
+
+public class Khaixxuan {
+    public void display(){
+        System.out.println("Nam moi binh an , som co cong viec on dinh ");
+    }
+}
---
+++ Baitap/src/Khaixuan/Main.java
@@ -0,0 +1,8 @@
+package Khaixuan;
+
+public class Main {
+    public static void main(String[] args) {
+        Khaixxuan n = new Khaixxuan();
+        n.display();
+    }
+}
